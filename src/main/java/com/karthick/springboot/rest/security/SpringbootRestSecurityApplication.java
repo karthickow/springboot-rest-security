bc9f7c727b81a231b037abbc102c64025503c70d
karthickow <karthickkrishnan24@gmail.com>
2023-11-29 01:04:44 +0530

    Initial commit

---
+++ src/main/java/com/karthick/springboot/rest/security/SpringbootRestSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.karthick.springboot.rest.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karthick/springboot/rest/security/SpringbootRestSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthick.springboot.rest.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
