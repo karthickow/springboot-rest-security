b993279c1dd66a6cc8fbc5dab031626152d7a01e
karthickow <karthickkrishnan24@gmail.com>
2023-12-13 01:20:35 +0530

    Added permit and deny all request code

--- src/main/java/com/karthick/springboot/rest/security/config/SecurityConfig.java
+++ src/main/java/com/karthick/springboot/rest/security/config/SecurityConfig.java
@@ -11,13 +11,27 @@ public class SecurityConfig {
 
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/my-account","/my-balance","/my-loans","/my-cards")
-				.authenticated()
-				.requestMatchers("/contact", "/notices")
-				.permitAll())
+		http.authorizeHttpRequests(requests -> requests
+				.requestMatchers("/my-account","/my-balance","/my-loans","/my-cards").authenticated()
+				.requestMatchers("/contact", "/notices").permitAll())
 				.formLogin(Customizer.withDefaults())
 				.httpBasic(Customizer.withDefaults());
 		return http.build();
+		
+		/*permit all requests
+		http.authorizeHttpRequests(requests -> requests
+				.anyRequest().permitAll())
+			.formLogin(Customizer.withDefaults())
+			.httpBasic(Customizer.withDefaults());
+		return http.build();
+		*/
+		
+		/*deny all requests
+		http.authorizeHttpRequests(requests -> requests
+				.anyRequest().denyAll())
+		.formLogin(Customizer.withDefaults())
+		.httpBasic(Customizer.withDefaults());
+		return http.build();
+		*/
 	}
 }
